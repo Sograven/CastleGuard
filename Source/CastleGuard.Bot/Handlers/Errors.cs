6b9695b9d42ee322198a0af0cef0a31b20a032ad
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2023-09-09 14:41:07 +0700

    Polling errors handler added (#14)

---
+++ Source/CastleGuard.Bot/Handlers/Errors.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot.Exceptions;
+using Telegram.Bot;
+
+namespace CastleGuard.Bot.Handlers
+{
+    internal class Errors
+    {
+        public static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
