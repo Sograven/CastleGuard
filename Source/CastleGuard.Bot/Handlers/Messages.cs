3ba71346d0c4a17b9af4014f2df3a7651245dda6
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2023-09-09 14:50:04 +0700

    Add update and message handlers (#14)

---
+++ Source/CastleGuard.Bot/Handlers/Messages.cs
@@ -0,0 +1,20 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CastleGuard.Bot.Handlers
+{
+    internal class Messages
+    {
+        public static async Task HandleMessagesAsync(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            if (message.Text is not null)
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: message.Text,
+                    cancellationToken: cancellationToken
+                    );
+            }
+        }
+    }
+}
---
+++ Source/CastleGuard.Bot/Handlers/Updates.cs
@@ -0,0 +1,16 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CastleGuard.Bot.Handlers
+{
+    internal class Updates
+    {
+        public static async Task HandleUpdatesAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is null)
+                return;
+
+            await Messages.HandleMessagesAsync(botClient, update.Message, cancellationToken);
+        }
+    }
+}
